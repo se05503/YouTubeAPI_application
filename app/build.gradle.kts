5630c7b83751b8eab48408829679101a12681860
Juseonghyun <wntjdgus99@naver.com>
2024-05-15 16:49:52 +0900

    build: Glide 추가
    dependencies Glide 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
     // Okhttp3
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    // Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
