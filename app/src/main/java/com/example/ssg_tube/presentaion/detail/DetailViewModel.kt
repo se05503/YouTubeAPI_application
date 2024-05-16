15fd4df2dc92ef28937c51e847e2b2cee9e93d11
AnJinHyuck <161280530+AnJinHyuck@users.noreply.github.com>
2024-05-16 21:18:24 +0900

    rename: 디테일 프래그먼트 xml 아이디 수정, 뷰모델 리스트명 DetailModel ->VideoModel로 수정

--- app/src/main/java/com/example/ssg_tube/presentaion/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/ssg_tube/presentaion/detail/DetailViewModel.kt
@@ -5,12 +5,11 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.ssg_tube.presentaion.model.DetailModel
-import com.google.gson.GsonBuilder
+import com.example.ssg_tube.presentaion.model.VideoModel
 
 class DetailViewModel : ViewModel() {
-    private val _likedItems = MutableLiveData<List<DetailModel>>()
-    val bookmarkedItems: LiveData<List<DetailModel>> get() = _likedItems
+    private val _likedItems = MutableLiveData<List<VideoModel>>()
+    val bookmarkedItems: LiveData<List<VideoModel>> get() = _likedItems
 
 
 
