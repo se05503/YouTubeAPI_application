667535a487256f63b900caa73cccc57365637152
AnJinHyuck <161280530+AnJinHyuck@users.noreply.github.com>
2024-05-16 17:24:57 +0900

    design: 디테일 프래그먼트 스크롤 뷰 적용

--- app/src/main/java/com/example/ssg_tube/presentaion/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/ssg_tube/presentaion/ui/detail/DetailViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.ssg_tube.presentaion.ui.detail
 
+import android.app.Activity
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ssg_tube.presentaion.model.DetailModel
+import com.google.gson.GsonBuilder
 
 class DetailViewModel : ViewModel() {
+    private val _likedItems = MutableLiveData<List<DetailModel>>()
+    val bookmarkedItems: LiveData<List<DetailModel>> get() = _likedItems
 
-    //데이터 클래스를 담는 라이브 데이터
-    //좋아요 처리
-    //프래그먼트
+
+
+
 }
