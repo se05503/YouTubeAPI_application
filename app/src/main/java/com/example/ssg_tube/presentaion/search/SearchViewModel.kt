65e9472d3bab6321f0aa07f6a3ba5cdbb96cc411
SEYOUNG PARK <seyeongb18@gmail.com>
2024-05-16 20:55:21 +0900

    fix: 모델 하나로 통합 후 SearchModel 을 VideoModel로 다 바꾸기

--- app/src/main/java/com/example/ssg_tube/presentaion/search/SearchViewModel.kt
+++ app/src/main/java/com/example/ssg_tube/presentaion/search/SearchViewModel.kt
@@ -5,19 +5,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.example.ssg_tube.data.model.SearchResponse
 import com.example.ssg_tube.data.remote.YouTubeAPI
-import com.example.ssg_tube.presentaion.model.SearchItemModel
+import com.example.ssg_tube.presentaion.model.VideoModel
 import kotlinx.coroutines.launch
-import javax.security.auth.callback.Callback
 
 class SearchViewModel(private val apiService: YouTubeAPI) : ViewModel() {
 
     // Livedata
-    private val _searchResults = MutableLiveData<List<SearchItemModel>>()
-    val searchResults: LiveData<List<SearchItemModel>> get() = _searchResults
+    private val _searchResults = MutableLiveData<List<VideoModel>>()
+    val searchResults: LiveData<List<VideoModel>> get() = _searchResults
 
-    var resItems: ArrayList<SearchItemModel> = ArrayList()
+    var resItems: ArrayList<VideoModel> = ArrayList()
 
     fun videoResults(query: String) { // videoSearch 를 불러오기 위해선 suspend 를 사용해야 한다.
         resItems.clear()
@@ -26,7 +24,7 @@ class SearchViewModel(private val apiService: YouTubeAPI) : ViewModel() {
             val requestResponse = apiService.videoSearch( // 비동기적으로 실행되기 때
                 part = "snippet",
                 query = query,
-                maxResults = 5,
+                maxResults = 20, // 해결 완료
                 order = "relevance",
                 type = "video",
                 videoType = "any"
@@ -37,7 +35,16 @@ class SearchViewModel(private val apiService: YouTubeAPI) : ViewModel() {
             for (item in items) {
                 val thumbnail = item.snippet.thumbnails["default"]!!.url
                 val title = item.snippet.title
-                resItems.add(SearchItemModel(title = title, url = thumbnail))
+                resItems.add(
+                    VideoModel(
+                        title = title,
+                        thumbnail = thumbnail,
+                        channelIcon = "",
+                        channelName = "",
+                        date = "",
+                        description = ""
+                    )
+                )
             }
             searchResult()
         }
