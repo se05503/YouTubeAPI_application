e8928f86563274c7bf597e3758975a57addbd2fc
Juseonghyun <wntjdgus99@naver.com>
2024-05-17 13:37:56 +0900

    feat: 카테고리 분류를 enum class로 사용하기위한 enum class 정의
    중복되는 코드를 없애기위해 카테고리 분류를 enum class로 사용하기위한 enum class 정의

---
+++ app/src/main/java/com/example/ssg_tube/presentaion/util/CategoryType.kt
@@ -0,0 +1,19 @@
+package com.example.ssg_tube.presentaion.util
+
+enum class CategoryType(val categoryId: String, val categoryName: String) {
+    MOVIE_AND_ANIMATION("1", "영화및 애니메이션"),
+    MUSIC("10", "음악"),
+    PETS_AND_ANIMALS("15", "애완동물및 동물"),
+    SPORTS("17", "스포츠"),
+    GAMING("20", "게임"),
+    NEWS_AND_POLITICS("25", "뉴스및 정치"),
+    SCIENCE_AND_TECHNOLOGY("28", "과학및 기술"),
+    MOVIES("30", "영화"),
+    HORROR("40", "공포");
+
+    companion object {
+        fun from(categoryName: String): CategoryType? {
+            return values().find { it.categoryName == categoryName }
+        }
+    }
+}
