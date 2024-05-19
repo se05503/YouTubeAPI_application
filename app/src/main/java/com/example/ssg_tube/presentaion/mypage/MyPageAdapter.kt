5c7329c727955fd44c2d4c09c76810b5c8a3b522
SEYOUNG PARK <seyeongb18@gmail.com>
2024-05-19 16:30:13 +0900

    enhancement: MyPageAdapter 에 onItemClick 을 인터페이스로 수정

--- app/src/main/java/com/example/ssg_tube/presentaion/mypage/MyPageAdapter.kt
+++ app/src/main/java/com/example/ssg_tube/presentaion/mypage/MyPageAdapter.kt
@@ -2,13 +2,9 @@ package com.example.ssg_tube.presentaion.mypage
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.ssg_tube.R
 import com.example.ssg_tube.databinding.SearchItemBinding
 import com.example.ssg_tube.presentaion.detail.DetailFragment
 import com.example.ssg_tube.presentaion.model.VideoModel
@@ -18,13 +14,18 @@ class MyPageAdapter(private val context: Context): RecyclerView.Adapter<MyPageAd
     // 좋아요한 아이템들
     var items = mutableListOf<VideoModel>()
 
-    inner class MyPageViewHolder(val binding: SearchItemBinding, val context: Context):RecyclerView.ViewHolder(binding.root),
-        View.OnClickListener {
-        var view = binding.clSearchView
+    interface OnItemClickListener {
+        fun onItemClick(item: VideoModel, position: Int)
+    }
 
-        init {
-            view.setOnClickListener(this)
-        }
+    private var clickListener: OnItemClickListener?= null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.clickListener = listener
+    }
+
+    inner class MyPageViewHolder(val binding: SearchItemBinding, val context: Context):RecyclerView.ViewHolder(binding.root) {
+        var view = binding.clSearchView
 
         fun bind(item: VideoModel) {
             binding.apply {
@@ -32,19 +33,6 @@ class MyPageAdapter(private val context: Context): RecyclerView.Adapter<MyPageAd
                     .load(item.thumbnail)
                     .into(ivThumbnail)
                 tvTitle.text = item.title
-            }
-        }
-
-        override fun onClick(v: View?) {
-            val position = adapterPosition.takeIf { it != RecyclerView.NO_POSITION } ?: return
-            val item = items[position]
-            val fragment = DetailFragment.newInstance(item)
-            val activity = context as AppCompatActivity // my page fragment 가 attach 된 메인 액티비티
-            // 현재 상황: 마이 페이지에서 아이템을 클릭 했을 때 디테일 프래그먼트로 이동한다
-            activity.supportFragmentManager.commit {
-                replace(R.id.flMain,fragment)
-                setReorderingAllowed(true)
-                addToBackStack(null)
             }
         }
     }
@@ -56,6 +44,9 @@ class MyPageAdapter(private val context: Context): RecyclerView.Adapter<MyPageAd
 
     override fun onBindViewHolder(holder: MyPageViewHolder, position: Int) {
         holder.bind(items[position])
+        holder.view.setOnClickListener {
+            clickListener?.onItemClick(items[position],position)
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/ssg_tube/presentaion/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/ssg_tube/presentaion/mypage/MyPageFragment.kt
@@ -2,13 +2,17 @@ package com.example.ssg_tube.presentaion.mypage
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.ssg_tube.R
 import com.example.ssg_tube.databinding.FragmentMyPageBinding
+import com.example.ssg_tube.presentaion.detail.DetailFragment
+import com.example.ssg_tube.presentaion.model.VideoModel
 import com.example.ssg_tube.presentaion.util.visible
 
 class MyPageFragment : Fragment() {
@@ -21,26 +25,47 @@ class MyPageFragment : Fragment() {
     private val viewModel: MyPageViewModel by viewModels()
 
     override fun onAttach(context: Context) {
+        Log.d("my page fragment","onAttach")
         super.onAttach(context)
         mContext = context
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("my page fragment","onCreate")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d("my page fragment","onCreateView")
         _binding = FragmentMyPageBinding.inflate(inflater,container,false)
         setUpView()
+        setUpListeners()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d("my page fragment","onViewCreated")
         observeViewModel()
+    }
+
+    private fun setUpListeners() {
+        adapter.setOnItemClickListener(object: MyPageAdapter.OnItemClickListener {
+            override fun onItemClick(item: VideoModel, position: Int) {
+                val fragment = DetailFragment.newInstance(item)
+                parentFragmentManager.beginTransaction() // 이거 나중에 util 쪽으로 빼놔도 좋을 것 같습니다
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .replace(R.id.flMain,fragment)
+                    .commit()
+
+
+            }
+
+        })
     }
 
     private fun setUpView() {
@@ -64,11 +89,13 @@ class MyPageFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        Log.d("my page fragment","onDestroyView")
         _binding = null
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d("my page fragment","onResume")
         (activity)?.visible()
     }
 }
