63ea65b0e21ab0455bd290543b5073c2d18edf91
Juseonghyun <wntjdgus99@naver.com>
2024-05-21 14:28:26 +0900

    feat: YoutubeRepositoryImpl에 getSearch함수 완성
    Search를 repository로 변환하기위해 YoutubeRepositoryImpl에 getSearch함수 완성

--- app/src/main/java/com/example/ssg_tube/data/repository/YoutubeRepositoryImpl.kt
+++ app/src/main/java/com/example/ssg_tube/data/repository/YoutubeRepositoryImpl.kt
@@ -4,10 +4,11 @@ import com.example.ssg_tube.data.remote.YouTubeAPI
 import com.example.ssg_tube.presentaion.model.ChannelInfo
 import com.example.ssg_tube.presentaion.model.VideoModel
 import com.example.ssg_tube.presentaion.repository.YoutubeRepository
+import com.example.ssg_tube.presentaion.util.FormatManager
 
-class YoutubeRepositoryImpl(private val api: YouTubeAPI) : YoutubeRepository {
+class YoutubeRepositoryImpl(private val apiService: YouTubeAPI) : YoutubeRepository {
     override suspend fun getPopularVideos(): List<VideoModel> {
-        val response = api.videoPopularList(
+        val response = apiService.videoPopularList(
             part = "snippet",
             chart = "mostPopular",
             regionCode = "KR"
@@ -27,7 +28,7 @@ class YoutubeRepositoryImpl(private val api: YouTubeAPI) : YoutubeRepository {
     }
 
     override suspend fun getCategoryVideos(categoryId: String): List<VideoModel> {
-        val response = api.videoCategoriesList(
+        val response = apiService.videoCategoriesList(
             part = "snippet",
             chart = "mostPopular",
             regionCode = "KR",
@@ -48,7 +49,7 @@ class YoutubeRepositoryImpl(private val api: YouTubeAPI) : YoutubeRepository {
     }
 
     override suspend fun getChannel(channelId: List<String>): List<ChannelInfo> {
-        val response = api.videoChannel(
+        val response = apiService.videoChannel(
             part = "snippet",
             id = channelId.joinToString(",")
         )
@@ -56,6 +57,29 @@ class YoutubeRepositoryImpl(private val api: YouTubeAPI) : YoutubeRepository {
             ChannelInfo(
                 id = item.id,
                 thumbnail = item.snippet.thumbnails.default.url
+            )
+        }
+    }
+
+    override suspend fun getSearch(query: String, order: String): List<VideoModel> {
+        val response = apiService.videoSearch(
+            part = "snippet",
+            query = query,
+            maxResults = 20,
+            order = order,
+            type = "video",
+            videoType = "any"
+        )
+        return response.items.map {item ->
+            VideoModel(
+                videoId = item.id.videoId,
+                title = item.snippet.title,
+                thumbnail = item.snippet.thumbnails.high.url,
+                channelIcon = "",
+                channelName = "",
+                date = FormatManager.dateFormat(item.snippet.date),
+                description = item.snippet.description,
+                channelId = item.snippet.channelId
             )
         }
     }
--- app/src/main/java/com/example/ssg_tube/presentaion/repository/YoutubeRepository.kt
+++ app/src/main/java/com/example/ssg_tube/presentaion/repository/YoutubeRepository.kt
@@ -7,4 +7,5 @@ interface YoutubeRepository {
     suspend fun getPopularVideos(): List<VideoModel>
     suspend fun getCategoryVideos(categoryId: String): List<VideoModel>
     suspend fun getChannel(channelId: List<String>): List<ChannelInfo>
+    suspend fun getSearch(query: String, order: String): List<VideoModel>
 }
