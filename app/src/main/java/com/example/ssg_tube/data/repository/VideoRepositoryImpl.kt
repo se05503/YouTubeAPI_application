260f98673c6c1df737f627ff7e1097b2547a7127
Juseonghyun <wntjdgus99@naver.com>
2024-05-21 11:55:49 +0900

    feat: repositoryImpl 완성 Home에서 쓰일 VideoRepositoryImpl 제작 완료

---
+++ app/src/main/java/com/example/ssg_tube/data/repository/VideoRepositoryImpl.kt
@@ -0,0 +1,62 @@
+package com.example.ssg_tube.data.repository
+
+import com.example.ssg_tube.data.remote.YouTubeAPI
+import com.example.ssg_tube.presentaion.model.ChannelInfo
+import com.example.ssg_tube.presentaion.model.VideoModel
+import com.example.ssg_tube.presentaion.repository.VideoRepository
+
+class VideoRepositoryImpl(private val api: YouTubeAPI) : VideoRepository {
+    override suspend fun getPopularVideos(): List<VideoModel> {
+        val response = api.videoPopularList(
+            part = "snippet",
+            chart = "mostPopular",
+            regionCode = "KR"
+        )
+        return response.items.map { item ->
+            VideoModel(
+                thumbnail = item.snippet.thumbnails.default.url,
+                title = item.snippet.title,
+                date = item.snippet.publishedAt,
+                channelIcon = "",
+                channelName = "",
+                description = item.snippet.description,
+                channelId = item.snippet.channelId,
+                videoId = item.id
+            )
+        }
+    }
+
+    override suspend fun getCategoryVideos(categoryId: String): List<VideoModel> {
+        val response = api.videoCategoriesList(
+            part = "snippet",
+            chart = "mostPopular",
+            regionCode = "KR",
+            videoCategoryId = categoryId
+        )
+        return response.items.map { item ->
+            VideoModel(
+                thumbnail = item.snippet.thumbnails.default.url,
+                title = item.snippet.title,
+                date = item.snippet.publishedAt,
+                channelIcon = "",
+                channelName = "",
+                description = item.snippet.description,
+                channelId = item.snippet.channelId,
+                videoId = item.id
+            )
+        }
+    }
+
+    override suspend fun getChannel(channelId: List<String>): List<ChannelInfo> {
+        val response = api.videoChannel(
+            part = "snippet",
+            id = channelId.joinToString(",")
+        )
+        return response.items.map { item ->
+            ChannelInfo(
+                id = item.id,
+                thumbnail = item.snippet.thumbnails.default.url
+            )
+        }
+    }
+}
