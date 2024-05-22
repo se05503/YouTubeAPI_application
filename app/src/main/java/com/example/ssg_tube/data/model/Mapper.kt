c893adcd7d69c570a538129ae1755014089aabd1
Juseonghyun <wntjdgus99@naver.com>
2024-05-22 14:28:34 +0900

    feat: Mapper에서 안쓰는 데이터는 Model에서 빈 문자열 처리
    Detail에서 Model의 값 중 빈문자열 처리한 값을 넣어줌

--- app/src/main/java/com/example/ssg_tube/data/model/Mapper.kt
+++ app/src/main/java/com/example/ssg_tube/data/model/Mapper.kt
@@ -4,20 +4,13 @@ import com.example.ssg_tube.presentaion.model.ChannelInfo
 import com.example.ssg_tube.presentaion.model.VideoModel
 import com.example.ssg_tube.presentaion.util.FormatManager
 
-// videoItem.videoMapper
-// 함수 이름 toVideoMapper
-// 확장함수를 쓰면 this로 접근 가능
-// 편하게 각 레이어별로 mapper가 있다고 생각해라.
-
 fun VideoItem.toDomainVideoModel(): VideoModel {
-    return VideoModel( // videoInfo
+    return VideoModel(
         videoId = this.id,
         thumbnail = this.snippet.thumbnails.default.url,
         title = this.snippet.title,
         date = this.snippet.publishedAt,
         description = this.snippet.description,
-        channelIcon = "",
-        channelName = "",
         channelId = this.snippet.channelId
     )
 }
@@ -30,12 +23,10 @@ fun ChannelItem.toDomainChannelModel(): ChannelInfo {
 }
 
 fun SearchItem.toDomainSearchModel(): VideoModel {
-    return VideoModel( // searchInfo
+    return VideoModel(
         videoId = this.id.videoId,
         title = this.snippet.title,
         thumbnail = this.snippet.thumbnails.high.url,
-        channelIcon = "",
-        channelName = "",
         date = FormatManager.dateFormat(this.snippet.date),
         description = this.snippet.description,
         channelId = this.snippet.channelId
