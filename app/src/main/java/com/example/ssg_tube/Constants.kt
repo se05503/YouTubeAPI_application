2f18a4667996188e7b162f215b509fc40b644709
SEYOUNG PARK <115936076+se05503@users.noreply.github.com>
2024-05-27 23:16:43 +0900

    Update Constants.kt
    API 키 뺐습니다.

--- app/src/main/java/com/example/ssg_tube/Constants.kt
+++ app/src/main/java/com/example/ssg_tube/Constants.kt
@@ -5,7 +5,7 @@ object Constants {
     const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
 
     // YoutubeAPI 인증키
-    const val AUTHORIZATION = "AIzaSyApH36Ag5Xwy2lRyRBCDcvfA-dQX9XTfRY"
+    const val AUTHORIZATION = ""
 
     // SharedPreference 파일 이름
     const val PREF = "pref"
